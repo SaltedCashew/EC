bd0a4a801d613d0747e55a44cd84405fd3fbbd4f
akuisara <zhang_qi@wheatoncollege.edu>
2015-01-28 00:20:51 +0000

    initial setup

---
+++ routes/index.js
@@ -0,0 +1,27 @@
+var express = require('express'),
+    apicache = require('apicache'),
+    fs = require('fs');
+
+var router = express.Router();
+
+var cache = apicache.middleware;
+
+/* GET home page. */
+router.get('/api/stops', cache('1 minute'), function(req, res, next) {
+  var content = JSON.parse(fs.readFileSync('data/stops.json'));
+  res.json(content);
+});
+
+// POST collection/id
+// app.post('/api/:collection/:id?', function(req, res, next) {
+//   // update model
+//   apicache.clear(req.params.collection);
+//   res.send(200);
+// });
+
+router.get('/', function(req, res) {
+
+    res.send('Hello World!');
+});
+
+module.exports = router;
