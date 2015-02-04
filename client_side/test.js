014eaa7758f91f68b570b941a2f33ecaf0ca02ca
akuisara <zhang_qi@wheatoncollege.edu>
2015-02-04 12:39:28 +0000

    More merge fix

---
+++ client_side/test.js
@@ -0,0 +1,9 @@
+var http = require('http');
+var fs = require('fs');
+var index = fs.readFileSync('helloworld.js');
+
+
+http.createServer(function (req, res) {
+	res.writeHead(200, {'Content-Type': 'text/plain'});
+	res.end(index);
+}).listen(9615);
