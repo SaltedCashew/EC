057ca077681f706e8d55aaa9f21594fd459abe91
akuisara <zhang_qi@wheatoncollege.edu>
2015-02-09 12:39:55 +0000

    added more routes

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
 var routes = require('./routes/index');
+var directions = require('./routes/directions');
 
 var app = express();
 
@@ -14,14 +15,19 @@ app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
 // uncomment after placing your favicon in /public
-// app.use(favicon(__dirname + '/public/favicon.ico'));
+app.use(favicon(path.join(__dirname,'public','favicon.ico')));
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'client_side')));
+app.use(function(req, res, next){
+    res.set("X-Powered-By", "EC Team Kirk");
+    next();
+});
 
 app.use('/', routes);
+app.use('/directions', directions);
 
 app.get('*', function(req, res) {
     res.send('Bad Route!');
--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,5 @@
 var express = require('express'),
-    apicache = require('apicache'),
-    fs = require('fs');
+    apicache = require('apicache');
 
 var request = require('request');
 
@@ -8,50 +7,61 @@ var router = express.Router();
 
 var cache = apicache.middleware;
 
-/* GET home page. */
-// router.get('/api/stops', cache('1 hour'), function(req, res, next) {
-//   var content = JSON.parse(fs.readFileSync('data/stops.json'));
-//   res.json(content);
-// });
-
-router.get('/api/stops', cache('5 minutes'), function(req, res, next) {
+router.get('/api/stops', cache('1 day'), function(req, res, next) {
     request('https://tfe-opendata.com/api/v1/stops', function (error, response, body) {
         if (!error && response.statusCode == 200) {
-            console.log("success!");// Print the google web page.
+            console.log("success!");
             res.json(JSON.parse(body));
         }
     });
 });
 
-router.get('/api/services', cache('5 minutes'), function(req, res, next) {
+router.get('/api/services', cache('1 day'), function(req, res, next) {
     request('https://tfe-opendata.com/api/v1/services', function (error, response, body) {
         if (!error && response.statusCode == 200) {
-            console.log("success!");// Print the google web page.
+            console.log("success!");
             res.json(JSON.parse(body));
         }
     });
 });
 
-router.get('/api/timetable/:stop_id', cache('5 minutes'), function(req, res, next) {
+router.get('/api/timetable/:stop_id', cache('1 day'), function(req, res, next) {
     var stopID = req.params.stop_id; // e.g., http://localhost:3000/api/timetable/36232897
     request('https://tfe-opendata.com/api/v1/timetables/'+stopID, function (error, response, body) {
         if (!error && response.statusCode == 200) {
-            console.log("success!");// Print the google web page.
+            console.log("success!");
             res.json(JSON.parse(body));
         }
     });
 });
 
-router.get('/api/journeys/:service_name', cache('5 minutes'), function(req, res, next) {
-    var busNumber = String(req.params.service_name); // e.g., http://localhost:3000/api/journeys/1
-    request('https://tfe-opendata.com/api/v1/timetables/'+busNumber, function (error, response, body) {
+router.get('/api/journeys/:service_name', cache('1 day'), function(req, res, next) {
+    var busNumber = req.params.service_name; // e.g., http://localhost:3000/api/journeys/1
+    request('https://tfe-opendata.com/api/v1/journeys/'+busNumber, function (error, response, body) {
         if (!error && response.statusCode == 200) {
-            console.log("success!");// Print the google web page.
+            console.log("success!");
             res.json(JSON.parse(body));
         }
     });
 });
 
+router.get('/api/status', cache('1 minute'), function(req, res, next) {
+    request('https://tfe-opendata.com/api/v1/status', function (error, response, body) {
+        if (!error && response.statusCode == 200) {
+            console.log("success!");
+            res.json(JSON.parse(body));
+        }
+    });
+});
+
+router.get('/api/vehicle_locations', cache('15 seconds'), function(req, res, next) {
+    request('https://tfe-opendata.com/api/v1/vehicle_locations', function (error, response, body) {
+        if (!error && response.statusCode == 200) {
+            console.log("success!");
+            res.json(JSON.parse(body));
+        }
+    });
+});
 
 // POST collection/id
 // app.post('/api/:collection/:id?', function(req, res, next) {
