b155b55c15e4081ac4eeecec1165c8859e7e9f6a
Qi Zhang <zhang_qi@wheatoncollege.edu>
2015-03-29 23:21:37 +0100

    add env.port and changed localhost to 5000

--- web.js
+++ web.js
@@ -1,6 +1,6 @@
 var app = require('./app');
 
-var server = app.listen(3000, function () {
+var server = app.listen(process.env.PORT || 5000, function () {
 
   var host = server.address().address;
   var port = server.address().port;
